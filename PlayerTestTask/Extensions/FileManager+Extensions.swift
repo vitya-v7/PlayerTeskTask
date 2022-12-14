9ea7c60c42fcc09a0ccd81e6de06c22b252038a0
vitya-v7 <deryabin-v7@mail.ru>
2022-12-13 22:33:54 -0800

    fixes

--- PlayerTestTask/Extensions/FileManager+Extensions.swift
+++ PlayerTestTask/Extensions/FileManager+Extensions.swift
@@ -5,15 +5,39 @@
 //  Created by Admin on 06.12.2022.
 //
 
-import Foundation
+import AVFoundation
+import UIKit
 
 extension FileManager {
     func getDocumentDirectoryURL() -> URL? {
-        return try? FileManager.default.url(
+        let url = try? FileManager.default.url(
             for: .documentDirectory,
                in: .userDomainMask,
                appropriateFor: nil,
                create: true
         )
+        guard let folderURL = url?.appendingPathComponent("MusicFilesStorage", isDirectory: true) else {
+            return nil
+        }
+        if !FileManager.default.fileExists(atPath: folderURL.path,
+                                           isDirectory: nil) {
+            do {
+                try FileManager.default.createDirectory(at: folderURL,
+                                                withIntermediateDirectories: false,
+                                                attributes: nil)
+            } catch {
+                if var topController = UIApplication.shared.keyWindow?.rootViewController {
+                    while let presentedViewController = topController.presentedViewController {
+                        topController = presentedViewController
+                    }
+                    
+                    AlertHelper().showSimpleAlert(inViewController: topController,
+                                                  withTitle: "Error",
+                                                  message: error.localizedDescription,
+                                                  confirmBlock: nil)
+                }
+            }
+        }
+        return folderURL
     }
 }
